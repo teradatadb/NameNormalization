dc73d3f6e95da6b59bb49958a19923327419d072
teradatadb <110453239+teradatadb@users.noreply.github.com>
2020-07-27 17:08:17 +0000

    Update workspace

--- app.py
+++ app.py
@@ -24,3 +24,6 @@ def main():
         """
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(
+            csv.encode()
+        ).decode()  # some strings <-> bytes conversions necessary here
+        return f'<a href="data:file/csv;base64,{b64}" download="copyright.csv">Download csv file</a>'
